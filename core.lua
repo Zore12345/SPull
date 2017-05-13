49e90f22c59181ec380d84061c3f9ce092c027c2
Zore12345 <makerbot11@gmail.com>
2017-05-13 19:24:03 -0400

    changed format of messages and other settings.
    1) condensed messages to allow for character comparisons instead of
    string comparisions.
    2) made sure that SimplePull was no longer being referanced and that
    referances are now to SPull.
    3) condensed setting.

--- core.lua
+++ core.lua
@@ -1,59 +1,92 @@
-local MSG_PREFIX = "SIMPLEPULL"
-RegisterAddonMessagePrefix(MSG_PREFIX)
-if not RegisterAddonMessagePrefix(MSG_PREFIX) then
+local SPULL_MSG_PREFIX = "SPULL"
+RegisterAddonMessagePrefix(SPULL_MSG_PREFIX)
+if not RegisterAddonMessagePrefix(SPULL_MSG_PREFIX) then
 	print("error unable to register prefix")
 end
 
 local defaults = {}
 defaults["DefaultPullTime"] = 5
-defaults["UseServerTime"] = false			--currently unimplemented but put feild in here for easy implementation.
-defaults["UseSyncPull"] = false				--currently unimplemented but put feild in here for easy implementation.
-defaults["ManualSyncOnly"] = false			--currently unimplemented but put feild in here for easy implementation.
+-- 0 = simple pull
+-- 1 = server pull
+-- 2 = sync pull
+defaults["PullType"] = 1
+defaults["UseBlizzardTimer"] = false		--currently unimplemented but put feild in here for easy implementation.
 defaults["UseRaidWarning"] = false			--currently unimplemented but put feild in here for easy implementation.
 defaults["UseVoice"] = false				--currently unimplemented but put feild in here for easy implementation.
-defaults["VoiceProfile"] = "MALE"			--currently unimplemented but put feild in here for easy implementation.
-defaults["Version"] = 0.15					--currently unimplemented but put feild in here for easy implementation.
+defaults["VoiceProfile"] = 'M'				--currently unimplemented but put feild in here for easy implementation.
+defaults["Version"] = 0.21					--currently unimplemented but put feild in here for easy implementation.
 
-local SimplePull = {}
-SimplePull.panel = CreateFrame( "Frame", "SimplePull", UIParent )
-SimplePull.panel.name = "|cFF5194ffSimple Pull|r"
-InterfaceOptions_AddCategory(SimplePull.panel)
+local SPull = {}
+SPull.panel = CreateFrame( "Frame", "SPull", UIParent )
+SPull.panel.name = "|cFF5194ffSPull|r"
+InterfaceOptions_AddCategory(SPull.panel)
 											--finish
 											--interface
 											--options
 											--menu
 
-if not type(SimplePullSettings) then
-    SimplePullSettings = defaults			--should make a deep copy that adds new setting if they are not present and app was updated.
+--if not type(SPullSettings) then
+    SPullSettings = defaults				--currently just overides all settings as changes to basic setting format might (and has) broken code.
+--elseif (SPullSettings["Version"]<defaults["Version"]) then
+--	progressiveSettingInit();
+--end
+
+function progressiveSettingInit()			--finish stub.
+
 end
 
-SLASH_SIMPLEPULL1, SLASH_SIMPLEPULL2 = '/simplepull', '/spull'
-local function handler(msg, editbox)
+function showSPullLoadMessage()
+	print("|cFF5194ffSPull(|r".. SPullSettings["Version"] .."|cFF5194ff)|r succesfully loaded.");
+end
+
+SLASH_SPULL1, SLASH_SPULL2 = '/simplepull', '/spull'
+local function slashHandler(msg, editbox)
 	if msg ~= '' then
 		local count = tonumber(msg)
 		if count ~= nil then
-			sendPull(count)
-		else
-			if string.lower(msg) == 'util' then
-				InterfaceOptionsFrame_OpenToCategory(SimplePull.panel)
-			end
+			formatSPull(count)
+		elseif string.lower(msg) == 'util' then
+			InterfaceOptionsFrame_OpenToCategory(SPull.panel)
 		end
 	else
-		pull(SimplePullSettings["DefaultPullTime"])
+		formatSPull(SPullSettings["DefaultPullTime"])
 	end
 end
-SlashCmdList["SIMPLEPULL"] = handler;
+SlashCmdList["SPULL"] = slashHandler;
 
-function SimplePull_ShowMessage()
-	print("|cFF5194ffSimplePull:|r succesfully loaded.")
+SLASH_HIDDENINTHECODE1 = '/hiddenspullslashcommandthatsreallylong.'
+local function slashHiddenStuff(msg, editbox)
+	print("|cFF00AA00You receive loot:|r |cFFFF8000|Hitem:18254::::::::::::|h[HOLY GRAIL]|h.|r");
+end
+SlashCmdList["HIDDENINTHECODE"] = slashHiddenStuff;
+
+--Message Format for SPull
+--  (type)#  -Format to send Pulls to other users.
+--    type:
+--      i = simple pull  -in this mode the number is pull length.
+--      e = serverPull  -in this mode the number is the end server time.
+--      y = syncPull  -in this mode the number is pull length.
+--  p  -used to ping user to determine time offset for syncpull.
+--  r  -used to reply to a ping.
+function formatSPull(number)
+	if SPullSettings["PullType"]==2 then
+		syncPullHandler(number);
+	else
+		print(SPullSettings["PullType"]==0 and 'i' or 'e'..number);
+		sendSPull(SPullSettings["PullType"]==0 and 'i' or 'e'..number);
+	end
 end
 
-function pull(number)					--finish
-	
+function syncPullHandler(number)		--todo:flush out
+
 end
 
-function sendPull(number)
-	SendAddonMessage(MSG_PREFIX, number, "RAID")
+function sendSPull(message)
+	SendAddonMessage(SPULL_MSG_PREFIX, message, "RAID");
+end
+
+function sendTargetedSPull(message,target)
+	SendAddonMessage(SPULL_MSG_PREFIX, message, "WHISPER", target);
 end
 
 if not eventFrame then
@@ -66,8 +99,8 @@ function eventhandler(self, event, prefix, message, channel, sender)
 	if realm==nil then
 		realm = GetRealmName()
 	end
-	if sender~=(name.."-"..realm) then
+	--if sender~=(name.."-"..realm) then
 		print("got " .. message .. " from " .. sender)
-	end
+	--end
 end
 eventFrame:SetScript("OnEvent", eventhandler)
